b7fe244c77233f75ba720e5c7f63235f9d7127cd
Alejandro Viesca <109549664+ViesK@users.noreply.github.com>
2025-02-20 12:01:38 +0000

    finish aplication

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,11 +5,70 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+@api.route('/register', methods=['POST'])
+def register():
+    
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    if not email or not password:
+        return jsonify({"msg": "missing data"}), 400
+    
+    exist = User.query.filter_by(email=email).first()
+    if exist:
+        return jsonify({"msg": "email token"}), 400
+
+    hashed_password = generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password, is_active=True)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    token = create_access_token(identity=str(new_user.id))
+
+    return jsonify({"msg": "ok", "token": token}), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    if not email or not password:
+        return jsonify({"msg": "missing data"}), 400
+    
+    exist = User.query.filter_by(email=email).first()
+    if not exist:
+        return jsonify({"msg": "no user found"}), 404
+
+    if not check_password_hash(exist.password, password):
+        return jsonify({"msg": "email or password wrong"}), 404
+
+    token = create_access_token(identity=str(exist.id))
+
+    return jsonify({"msg": "ok", "token": token}), 200
+
+
+@api.route('/protected', methods=['GET'])
+@jwt_required()
+def protected():
+    id = get_jwt_identity()
+    user = User.query.get(id)
+    
+    return jsonify({"msg": 'OK', 'user': user.serialize()})
+
+
+
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,6 +2,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
+
+			auth: localStorage.getItem('token') ||false,
+
 			demo: [
 				{
 					title: "FIRST",
@@ -17,6 +20,88 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
+			getUserData: async () => {
+				try {
+					const resp = await fetch('https://laughing-pancake-p4pgvjjg656f6674-3001.app.github.dev/api/protected', 
+					{
+						method: 'GET',
+						headers:{
+							'Content-type': 'application/json',
+							'Authorization': `Bearer ${localStorage.getItem('token')}`
+						},
+					})
+
+					if(!resp.ok) throw new Error('error registering')
+
+					const data = await resp.json()
+					console.log(data)
+
+					setStore({user: data.user})
+
+				} catch (error) {
+					console.log(error)
+				}
+			},
+
+
+			register: async formData => {
+				try {
+					const resp = await fetch('https://laughing-pancake-p4pgvjjg656f6674-3001.app.github.dev/api/register', 
+					{
+						method: 'POST',
+						headers:{'Content-type': 'application/json'},
+						body: JSON.stringify(formData)
+					})
+
+					if(!resp.ok) throw new Error('error registering')
+
+					const data = await resp.json()
+					console.log(data)
+
+					localStorage.setItem('token', data.token)
+					setStore({auth: true, token: data.token})
+
+				} catch (error) {
+					console.log(error)
+				}
+
+			},
+
+
+			login: async formData => {
+				try {
+					const resp = await fetch('https://laughing-pancake-p4pgvjjg656f6674-3001.app.github.dev/api/login',
+					{
+						method: 'POST',
+						headers:{'Content-type': 'application/json'},
+						body: JSON.stringify(formData)
+					})
+
+					if(!resp.ok) throw new Error('error in login')
+
+					const data = await resp.json()
+					console.log(data)
+
+					localStorage.setItem('token', data.token);
+					setStore({auth: true, token: data.token});
+
+				} catch (error) {
+					console.log(error)
+				}
+			},
+			
+			
+			logout:  () => {
+				localStorage.removeItem('token')
+				setStore({auth: true, false: null});
+			},
+			
+			
+			
+			
+			
+			
+			
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
